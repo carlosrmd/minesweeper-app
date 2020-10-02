f319b332aa4ffa6dbefee6a92446155fa1aaac41
cr-martinez <carlosrmd94@gmail.com>
2020-10-02 00:42:37 -0300

    including constants files with constant strings

--- minesweeper/constants.py
+++ minesweeper/constants.py
@@ -1,4 +1,29 @@
+# Actions
 PAUSE_GAME = "pause"
 RESUME_GAME = "resume"
 END_GAME = "end"
 MOVE = "move"
+
+# Statuses
+STATUS_PAUSED = "PAUSED"
+STATUS_ACTIVE = "ACTIVE"
+STATUS_FINISHED = "FINISHED"
+
+# Results
+RESULT_FINISHED_BY_USER = "Finished by user"
+RESULT_GAME_LOST = "Game lost"
+RESULT_GAME_VICTORY = "Victory!"
+
+MOVE_RED_FLAG = "FLAG"
+MOVE_QUESTION_MARK = "QUESTION_MARK"
+MOVE_CLICK = "CLICK"
+
+flag_cell = {
+    MOVE_RED_FLAG: "F",
+    MOVE_QUESTION_MARK: "Q"
+}
+
+MSG_MISSING_FIELDS = "Error: missing required fields."
+MSG_GAME_NOT_FOUND = "Error: game not found."
+MSG_COMMAND_UNRECOGNIZED = "Error: command unrecognized."
+MSG_USER_ALREADY_EXISTS = "Error: user name already exists."
